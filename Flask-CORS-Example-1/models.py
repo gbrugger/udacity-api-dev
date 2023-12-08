223926cf132938f3822e6713380d4211978f42a0
Guilherme Brugger <guilherme.brugger@gmail.com>
2023-12-08 09:50:01 -0300

    Initial version

--- Flask-CORS-Example-1/models.py
+++ Flask-CORS-Example-1/models.py
@@ -33,27 +33,22 @@ class Plant(db.Model):
     is_poisonous = Column(Boolean)
     primary_color = Column(String)
 
-
     def __init__(self, name, scientific_name, is_poisonous, primary_color):
         self.name = name
         self.scientific_name = scientific_name
         self.is_poisonous = is_poisonous
         self.primary_color = primary_color
 
-
     def insert(self):
         db.session.add(self)
         db.session.commit()
 
-
     def update(self):
         db.session.commit()
-
 
     def delete(self):
         db.session.delete(self)
         db.session.commit()
-
 
     def format(self):
         return {
